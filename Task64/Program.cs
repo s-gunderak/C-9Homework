3e2d04653ded0eda60249ea6d6d4a4982b08a211
gunderak <sergey.gund@gmail.com>
2023-03-14 18:30:35 +0300

    add gitignore + Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string NumbersRec(int n)
+{
+    if (n < 1) return String.Empty;
+    else return $"{n} " + NumbersRec(n - 1);
+}
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(NumbersRec(n));
